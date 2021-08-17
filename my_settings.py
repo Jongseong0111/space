dabad4d007421ee44662fbc9110860074ad9bda0
임종성 <qkqndudnxld@gmail.com>
2021-08-18 01:13:00 +0900

    initial

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'space',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'asdfasdf'
+
